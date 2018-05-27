b412a591e02ff7c7f2cce2fd0edb500c4456b627
briandhkim <w1057216@gmail.com>
2018-05-26 19:23:17 -0700

    initial test for retrieving all data success

--- table/access.php
+++ table/access.php
@@ -9,10 +9,10 @@ $output = [
 	'errors' => [],
 ];
 
-if(empty($_POST['action'])){
-	$output['errors'][] = 'action not provided';
-	output_and_exit($output);
-}
+// if(empty($_POST['action']) or empty($_GET['action']) ){
+// 	$output['errors'][] = 'action not provided';
+// 	output_and_exit($output);
+// }
 
 function output_and_exit($output){
 	$json_output = json_encode($output);
@@ -21,10 +21,39 @@ function output_and_exit($output){
 	exit();
 }
 
-switch($_POST['action']){
+if($_SERVER['REQUEST_METHOD'] === 'GET'){
+	if(empty($_GET['action']) ){
+		$output['errors'][] = 'action not provided';
+		output_and_exit($output);
+	}
+	switch($_GET['action']){
+		case 'get_all_data':
+			include('get_all_data.php');
+			break;
+		default:
+			$output['errors'][] = 'invalid action';
+	}
 	
-	default:
-		$output['errors'][] = 'invalid action';
+
+}else if($_SERVER['REQUEST_METHOD'] === 'POST'){
+	if(empty($_POST['action'])){
+		$output['errors'][] = 'action not provided';
+		output_and_exit($output);
+	}
+
+	switch($_POST['action']){
+		// case 'get_all_data':
+		// include('get_all_data.php');
+		// 	break;
+		default:
+			$output['errors'][] = 'invalid action';
+	}
+
+}else{
+	$output['errors'][] = 'invalid method';
+	output_and_exit($output);
 }
+
+
 output_and_exit($output);
 ?>
