fba9d7395250fd52fd8d7e952e9e2c9d67dfd1ca
briandhkim <w1057216@gmail.com>
2018-05-27 19:00:19 -0700

    cors allow headers content type enable

--- table/access.php
+++ table/access.php
@@ -1,6 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');  
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Headers: Content-Type');
 session_start();
 $LOCAL_ACCESS = true;
 
